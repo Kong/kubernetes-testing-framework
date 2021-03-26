81748c8f2ddaa72dbd0584070428efbc20510170
Shane Utt <shaneutt@users.noreply.github.com>
2021-03-26 09:56:10 -0400

    feat: add *rest.Config to kind.Cluster (#11)

--- pkg/kind/cluster.go
+++ pkg/kind/cluster.go
@@ -2,6 +2,7 @@ package kind
 
 import (
 	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
 )
 
 // -----------------------------------------------------------------------------
@@ -15,6 +16,9 @@ type Cluster interface {
 
 	// Client is the configured *kubernetes.Clientset which can be used to access the Cluster's API
 	Client() *kubernetes.Clientset
+
+	// Config provides the *rest.Config for the cluster which is convenient for initiating custom kubernetes.Clientsets.
+	Config() *rest.Config
 
 	// Cleanup obliterates the cluster and all of its resources, leaving no garbage behind, unless `KIND_KEEP_CLUSTER` is set.
 	Cleanup() error
