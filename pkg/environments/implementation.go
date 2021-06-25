57957e7174973ff59981ef65bfd22e13cb82867e
Shane Utt <shaneutt@linux.com>
2021-06-25 15:16:34 -0400

    feat: add testing environments

---
+++ pkg/environments/implementation.go
@@ -0,0 +1,104 @@
+package environments
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	appsv1 "k8s.io/api/apps/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+
+	"github.com/kong/kubernetes-testing-framework/pkg/clusters"
+)
+
+// -----------------------------------------------------------------------------
+// Test Environment - Implementation
+// -----------------------------------------------------------------------------
+
+// environment is the default KTF Environment used for testing Kubernetes ingress.
+type environment struct {
+	name    string
+	cluster clusters.Cluster
+}
+
+func (env *environment) Name() string {
+	return env.name
+}
+
+func (env *environment) Cluster() clusters.Cluster {
+	return env.cluster
+}
+
+func (env *environment) Cleanup(ctx context.Context) error {
+	return env.Cluster().Cleanup(ctx)
+}
+
+func (env *environment) Ready(ctx context.Context) (waitForObjects []runtime.Object, ready bool, err error) {
+	var deployments *appsv1.DeploymentList
+	var daemonsets *appsv1.DaemonSetList
+
+	deployments, err = env.Cluster().Client().AppsV1().Deployments("kube-system").List(ctx, metav1.ListOptions{})
+	if err != nil {
+		return
+	}
+
+	daemonsets, err = env.Cluster().Client().AppsV1().DaemonSets("kube-system").List(ctx, metav1.ListOptions{})
+	if err != nil {
+		return
+	}
+
+	for _, deployment := range deployments.Items {
+		if deployment.Status.ReadyReplicas != *deployment.Spec.Replicas {
+			waitForObjects = append(waitForObjects, &deployment)
+		}
+	}
+
+	for _, daemonset := range daemonsets.Items {
+		if daemonset.Status.NumberUnavailable > 0 {
+			waitForObjects = append(waitForObjects, &daemonset)
+		}
+	}
+
+	for _, addon := range env.Cluster().ListAddons() {
+		waitForAddonObjects := make([]runtime.Object, 0)
+		waitForAddonObjects, ready, err = addon.Ready(ctx, env.Cluster())
+		if err != nil {
+			return
+		}
+		waitForObjects = append(waitForObjects, waitForAddonObjects...)
+	}
+
+	ready = len(waitForObjects) == 0
+	return
+}
+
+func (env *environment) WaitForReady(ctx context.Context) chan error {
+	errs := make(chan error)
+
+	go func() {
+		waitForObjects := make([]runtime.Object, 0)
+		for {
+			select {
+			case <-ctx.Done():
+				errs <- fmt.Errorf("context done before environment was ready (remaining objects %+v): %w", waitForObjects, ctx.Err())
+				return
+			default:
+				var ready bool
+				var err error
+				waitForObjects, ready, err = env.Ready(ctx)
+				if err != nil {
+					errs <- err
+					return
+				}
+				if ready {
+					errs <- nil
+					return
+				}
+				time.Sleep(time.Millisecond * 200)
+			}
+		}
+	}()
+
+	return errs
+}
