740e946c046fd3830ecf20dd6cfac3ce2f44d4cb
Shane Utt <shaneutt@linux.com>
2021-06-25 15:17:54 -0400

    feat: add a kind-based cluster implementation

---
+++ pkg/clusters/types/kind/cluster.go
@@ -0,0 +1,151 @@
+package kind
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"sync"
+
+	"github.com/google/uuid"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+
+	"github.com/kong/kubernetes-testing-framework/pkg/clusters"
+)
+
+// -----------------------------------------------------------------------------
+// Kind Cluster
+// -----------------------------------------------------------------------------
+
+const (
+	// KindClusterType indicates that the Kubernetes cluster was provisioned by Kind.
+	KindClusterType clusters.Type = "kind"
+
+	// EnvKeepCluster is the environment variable that can be set to "true" in order
+	// to circumvent teardown during cleanup of clusters in order to allow a user to inspect them instead.
+	EnvKeepCluster = "KIND_KEEP_CLUSTER"
+
+	// DefaultKindDockerNetwork is the Docker network that a kind cluster uses by default.
+	DefaultKindDockerNetwork = "kind"
+)
+
+// kindCluster is a clusters.Cluster implementation backed by Kubernetes In Docker (KIND)
+type kindCluster struct {
+	name   string
+	client *kubernetes.Clientset
+	cfg    *rest.Config
+	addons clusters.Addons
+	l      *sync.RWMutex
+}
+
+// New provides a new clusters.Cluster backed by a Kind based Kubernetes Cluster.
+func New(ctx context.Context) (clusters.Cluster, error) {
+	name := uuid.NewString()
+	err := createCluster(name)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create cluster %s: %w", name, err)
+	}
+
+	cfg, kc, err := clientForCluster(name)
+	if err != nil {
+		return nil, err
+	}
+
+	return &kindCluster{
+		name:   name,
+		client: kc,
+		cfg:    cfg,
+		l:      &sync.RWMutex{},
+		addons: make(clusters.Addons),
+	}, nil
+}
+
+// -----------------------------------------------------------------------------
+// Kind Cluster - Cluster Implementation
+// -----------------------------------------------------------------------------
+
+func (c *kindCluster) Name() string {
+	return c.name
+}
+
+func (c *kindCluster) Type() clusters.Type {
+	return KindClusterType
+}
+
+func (c *kindCluster) Cleanup(ctx context.Context) error {
+	c.l.Lock()
+	defer c.l.Unlock()
+
+	if os.Getenv(EnvKeepCluster) == "" {
+		return deleteKindCluster(ctx, c.name)
+	}
+
+	return nil
+}
+
+func (c *kindCluster) Client() *kubernetes.Clientset {
+	return c.client
+}
+
+func (c *kindCluster) Config() *rest.Config {
+	return c.cfg
+}
+
+func (c *kindCluster) GetAddon(name clusters.AddonName) (clusters.Addon, error) {
+	c.l.RLock()
+	defer c.l.RUnlock()
+
+	for addonName, addon := range c.addons {
+		if addonName == name {
+			return addon, nil
+		}
+	}
+
+	return nil, fmt.Errorf("addon %s not found", name)
+}
+
+func (c *kindCluster) ListAddons() []clusters.Addon {
+	c.l.RLock()
+	defer c.l.RUnlock()
+
+	addonList := make([]clusters.Addon, 0, len(c.addons))
+	for _, v := range c.addons {
+		addonList = append(addonList, v)
+	}
+
+	return addonList
+}
+
+func (c *kindCluster) DeployAddon(ctx context.Context, addon clusters.Addon) error {
+	c.l.Lock()
+	defer c.l.Unlock()
+
+	if _, ok := c.addons[addon.Name()]; ok {
+		return fmt.Errorf("addon component %s is already loaded into cluster %s", addon.Name(), c.Name())
+	}
+
+	if err := addon.Deploy(ctx, c); err != nil {
+		return err
+	}
+
+	c.addons[addon.Name()] = addon
+
+	return nil
+}
+
+func (c *kindCluster) DeleteAddon(ctx context.Context, addon clusters.Addon) error {
+	c.l.Lock()
+	defer c.l.Unlock()
+
+	if _, ok := c.addons[addon.Name()]; !ok {
+		return nil
+	}
+
+	if err := addon.Delete(ctx, c); err != nil {
+		return err
+	}
+
+	delete(c.addons, addon.Name())
+
+	return nil
+}
---
+++ pkg/clusters/types/kind/utils.go
@@ -0,0 +1,89 @@
+package kind
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"io"
+	"os"
+	"os/exec"
+	"sync"
+
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+
+	"github.com/kong/kubernetes-testing-framework/pkg/clusters"
+)
+
+// -----------------------------------------------------------------------------
+// Public Functions - Existing Cluster
+// -----------------------------------------------------------------------------
+
+// NewFromExisting provides a Cluster object for a given kind cluster by name.
+func NewFromExisting(name string) (clusters.Cluster, error) {
+	cfg, kc, err := clientForCluster(name)
+	if err != nil {
+		return nil, err
+	}
+	return &kindCluster{
+		name:   name,
+		client: kc,
+		cfg:    cfg,
+		l:      &sync.RWMutex{},
+		addons: make(clusters.Addons),
+	}, nil
+}
+
+// -----------------------------------------------------------------------------
+// Private Functions - Cluster Management
+// -----------------------------------------------------------------------------
+
+// createCluster creates a new cluster using Kubernetes in Docker (KIND).
+func createCluster(name string) error {
+	stderr := new(bytes.Buffer)
+	cmd := exec.Command("kind", "create", "cluster", "--name", name)
+	cmd.Stdout = io.Discard
+	cmd.Stderr = stderr
+	if err := cmd.Run(); err != nil {
+		return fmt.Errorf("%s: %w", stderr.String(), err)
+	}
+	return nil
+}
+
+// deleteKindCluster deletes an existing KIND cluster.
+func deleteKindCluster(ctx context.Context, name string) error {
+	stderr := new(bytes.Buffer)
+	cmd := exec.CommandContext(ctx, "kind", "delete", "cluster", "--name", name)
+	cmd.Stdout = io.Discard
+	cmd.Stderr = stderr
+	if err := cmd.Run(); err != nil {
+		return fmt.Errorf("%s: %w", stderr.String(), err)
+	}
+
+	return nil
+}
+
+// clientForCluster provides a *kubernetes.Clientset for a KIND cluster provided the cluster name.
+func clientForCluster(name string) (*rest.Config, *kubernetes.Clientset, error) {
+	kubeconfig := new(bytes.Buffer)
+	cmd := exec.Command("kind", "get", "kubeconfig", "--name", name)
+	cmd.Stdout = kubeconfig
+	cmd.Stderr = os.Stderr
+	if err := cmd.Run(); err != nil {
+		return nil, nil, err
+	}
+
+	clientCfg, err := clientcmd.NewClientConfigFromBytes(kubeconfig.Bytes())
+	if err != nil {
+		return nil, nil, err
+	}
+
+	cfg, err := clientCfg.ClientConfig()
+	if err != nil {
+		return nil, nil, err
+	}
+
+	clientset, err := kubernetes.NewForConfig(cfg)
+	return cfg, clientset, err
+}
