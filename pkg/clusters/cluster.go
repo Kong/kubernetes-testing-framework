f3b58fc2f013df9b372a7fb4a9219f8335d621d0
Shane Utt <shaneutt@linux.com>
2021-06-25 15:17:40 -0400

    feat: add test clusters

---
+++ pkg/clusters/cluster.go
@@ -0,0 +1,45 @@
+package clusters
+
+import (
+	"context"
+
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+)
+
+// -----------------------------------------------------------------------------
+// Public Types - Cluster Interface
+// -----------------------------------------------------------------------------
+
+// Type indicates the type of Kubernetes cluster (e.g. Kind, GKE, e.t.c.)
+type Type string
+
+// Cluster objects represent a running Kubernetes cluster.
+type Cluster interface {
+	// Name indicates the unique name of the running cluster.
+	Name() string
+
+	// Type indicates the type of Kubernetes Cluster (e.g. Kind, GKE, e.t.c.)
+	Type() Type
+
+	// Client is the configured *kubernetes.Clientset which can be used to access the Cluster's API
+	Client() *kubernetes.Clientset
+
+	// Config provides the *rest.Config for the cluster which is convenient for initiating custom kubernetes.Clientsets.
+	Config() *rest.Config
+
+	// Cleanup performance any cleanup and teardown needed to destroy the cluster.
+	Cleanup(ctx context.Context) error
+
+	// GetAddon retrieves and Addon object from the cluster if that addon was previously loaded.
+	GetAddon(name AddonName) (Addon, error)
+
+	// ListAddons lists the addon components currently loaded into the cluster.
+	ListAddons() []Addon
+
+	// DeployAddon deploys a new addon component to the cluster.
+	DeployAddon(ctx context.Context, addon Addon) error
+
+	// DeleteAddon removes an existing cluster Addon.
+	DeleteAddon(ctx context.Context, addon Addon) error
+}
