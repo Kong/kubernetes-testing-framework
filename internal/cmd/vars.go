35ca6e48a79bc4842b7b21bc6f31e8a778ac2b9b
Shane Utt <shaneutt@linux.com>
2021-07-12 12:04:01 -0400

    feat: add cli for env create and delete

---
+++ internal/cmd/vars.go
@@ -0,0 +1,21 @@
+package cmd
+
+import "time"
+
+// -----------------------------------------------------------------------------
+// Environments Vars
+// -----------------------------------------------------------------------------
+
+const (
+	// DefaultEnvironmentName indicates the name that will be provided for
+	// created environments if no other name is provided
+	DefaultEnvironmentName = "kong-testing-environment"
+
+	// EnvironmentCreateTimeout indicates the amount of time maximum that should
+	// be allowed to wait for a test environment to finish creating.
+	EnvironmentCreateTimeout = time.Minute * 5
+
+	// EnvironmentDeleteTimeout indicates the amount of time maximum that should
+	// be allowed to wait for a test environment to delete successfully.
+	EnvironmentDeleteTimeout = time.Minute * 1
+)
