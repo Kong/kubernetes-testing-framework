4f3aab30421dd857e6b79dfc9bfc2ae4e791fdbf
Shane Utt <shaneutt@linux.com>
2021-07-28 10:50:24 -0400

    chore: fully internalize cli components

--- internal/cmd/ktf/vars.go
+++ internal/cmd/ktf/vars.go
@@ -1,4 +1,4 @@
-package cmd
+package ktf
 
 import "time"
 
---
+++ internal/cmd/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/kong/kubernetes-testing-framework/internal/cmd/ktf"
+
+func main() {
+	ktf.Execute()
+}
