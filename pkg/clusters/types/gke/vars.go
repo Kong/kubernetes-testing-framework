9be39a4f2c5a5796b3e17d17982b78ac94836c05
Shane Utt <shaneutt@linux.com>
2021-07-29 12:10:39 -0400

    chore: move gke var names into gke pkg

--- pkg/clusters/types/gke/vars.go
+++ pkg/clusters/types/gke/vars.go
@@ -15,6 +15,15 @@ const (
 	// GKEClusterType indicates that the Kubernetes cluster was provisioned by Google Kubernetes Engine (GKE)
 	GKEClusterType clusters.Type = "gke"
 
+	// GKECredsVar indicates the environment variable used to provide auth credentials for GKE
+	GKECredsVar = "GOOGLE_APPLICATION_CREDENTIALS"
+
+	// GKEProjectVar indicates the environment variable used to provide a default gcloud project
+	GKEProjectVar = "GOOGLE_PROJECT"
+
+	// GKELocationVar indicates the environment variable used to provide a default gcloud region
+	GKELocationVar = "GOOGLE_LOCATION"
+
 	// waitForClusterTick indicates the number of seconds to wait between cluster checks
 	// when deploying a new GKE cluster.
 	waitForClusterTick = time.Second * 3
