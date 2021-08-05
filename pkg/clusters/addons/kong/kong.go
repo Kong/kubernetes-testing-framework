a9a3949d5fcff0334e1abbb0cf0e60f188396309
Shane Utt <shaneutt@linux.com>
2021-08-05 09:49:03 -0400

    fix: kong addon deployment idemopotence (#81)
    At times the repo add command might throw an error
    even if ultimately the repo is already there.
    
    This patch adds the `--force-overwrite` options when
    adding the kong repo to update it to the latest value
    regardless of whether or not it already existed.

--- pkg/clusters/addons/kong/kong.go
+++ pkg/clusters/addons/kong/kong.go
@@ -110,9 +110,9 @@ func (a *Addon) Deploy(ctx context.Context, cluster clusters.Cluster) error {
 	}
 	defer os.Remove(kubeconfig.Name())
 
-	// ensure the repo is added
+	// ensure the repo exists
 	stderr := new(bytes.Buffer)
-	cmd := exec.CommandContext(ctx, "helm", "--kubeconfig", kubeconfig.Name(), "repo", "add", "kong", KongHelmRepoURL) //nolint:gosec
+	cmd := exec.CommandContext(ctx, "helm", "--kubeconfig", kubeconfig.Name(), "repo", "add", "--force-update", "kong", KongHelmRepoURL) //nolint:gosec
 	cmd.Stdout = io.Discard
 	cmd.Stderr = stderr
 	if err := cmd.Run(); err != nil {
