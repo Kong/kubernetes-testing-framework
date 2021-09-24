0ea817c9a9db0a5210c355f13a4a84f831079786
Shane Utt <shaneutt@linux.com>
2021-09-24 15:32:34 -0400

    feat: add namespace readiness check utility

---
+++ internal/utils/namespaces.go
@@ -0,0 +1,66 @@
+package utils
+
+import (
+	"context"
+
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+
+	"github.com/kong/kubernetes-testing-framework/pkg/clusters"
+)
+
+// IsNamespaceReady checks for all Daemonsets, Deployment and Services
+// in a given namespace to see if they are ready. It reports on readiness
+// but also will provide a list of runtime objects that are being waited
+// on still if the namespace is not ready yet.
+//
+// Keep in mind that this specifically checks on readiness, not availability
+// (uptime) for speed reasons during tests.
+func IsNamespaceReady(ctx context.Context, cluster clusters.Cluster, namespace string) (waitForObjects []runtime.Object, ready bool, err error) {
+	// check daemonsets for readiness
+	var daemonsets *appsv1.DaemonSetList
+	daemonsets, err = cluster.Client().AppsV1().DaemonSets(namespace).List(ctx, metav1.ListOptions{})
+	if err != nil {
+		return
+	}
+
+	for i := 0; i < len(daemonsets.Items); i++ {
+		daemonset := &(daemonsets.Items[i])
+		if daemonset.Status.NumberReady < 1 {
+			waitForObjects = append(waitForObjects, daemonset)
+		}
+	}
+
+	// check deployments for readiness
+	var deployments *appsv1.DeploymentList
+	deployments, err = cluster.Client().AppsV1().Deployments(namespace).List(ctx, metav1.ListOptions{})
+	if err != nil {
+		return
+	}
+
+	for i := 0; i < len(deployments.Items); i++ {
+		deployment := &(deployments.Items[i])
+		if deployment.Status.ReadyReplicas != *deployment.Spec.Replicas {
+			waitForObjects = append(waitForObjects, deployment)
+		}
+	}
+
+	// check services for readiness
+	var services *corev1.ServiceList
+	services, err = cluster.Client().CoreV1().Services(namespace).List(ctx, metav1.ListOptions{})
+	if err != nil {
+		return
+	}
+
+	for i := 0; i < len(services.Items); i++ {
+		service := &(services.Items[i])
+		if service.Spec.Type == corev1.ServiceTypeLoadBalancer && len(service.Status.LoadBalancer.Ingress) < 1 {
+			waitForObjects = append(waitForObjects, service)
+		}
+	}
+
+	ready = len(waitForObjects) == 0
+	return
+}
