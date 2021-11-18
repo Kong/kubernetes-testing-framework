4c5baa4d0c4ec9dbdc10a6c04df7b6c0103e3fc2
Travis Raines <traines@konghq.com>
2021-11-18 11:53:41 -0800

    feat: add load image addon (#151)
    Add an addon to load images from the local Docker environment into the 
    test cluster. Currently only supports KIND.

---
+++ pkg/clusters/addons/loadimage/addon.go
@@ -0,0 +1,68 @@
+package loadimage
+
+import (
+	"context"
+	"fmt"
+
+	"k8s.io/apimachinery/pkg/runtime"
+
+	"github.com/kong/kubernetes-testing-framework/pkg/clusters"
+	"github.com/kong/kubernetes-testing-framework/pkg/clusters/types/kind"
+)
+
+// -----------------------------------------------------------------------------
+// CertManager Addon
+// -----------------------------------------------------------------------------
+
+const (
+	// AddonName indicates the unique name of this addon.
+	AddonName clusters.AddonName = "loadimage"
+)
+
+type Addon struct {
+	image  string
+	loaded bool
+}
+
+func New() clusters.Addon {
+	return &Addon{}
+}
+
+// -----------------------------------------------------------------------------
+// CertManager Addon - Addon Implementation
+// -----------------------------------------------------------------------------
+
+func (a *Addon) Name() clusters.AddonName {
+	return AddonName
+}
+
+func (a *Addon) Deploy(ctx context.Context, cluster clusters.Cluster) error {
+	switch ctype := cluster.Type(); ctype {
+	case kind.KindClusterType:
+		if err := a.loadIntoKind(cluster); err != nil {
+			return err
+		}
+	default:
+		return fmt.Errorf("loadimage addon is not supported by cluster type '%v'", cluster.Type())
+	}
+
+	return nil
+}
+
+func (a *Addon) Delete(ctx context.Context, cluster clusters.Cluster) error {
+	switch ctype := cluster.Type(); ctype {
+	case kind.KindClusterType:
+		// per https://github.com/kubernetes-sigs/kind/issues/658 this is basically impossible
+		// we lie here, because we want to mask this error. not deleting an image from KIND is benign:
+		// you either don't use it after (in which case you shouldn't care that it's still present) or
+		// load another image with the same name (in which case the name will point to the new image)
+		return nil
+	default:
+		return fmt.Errorf("loadimage addon is not supported by cluster type '%v'", cluster.Type())
+	}
+}
+
+func (a *Addon) Ready(ctx context.Context, cluster clusters.Cluster) ([]runtime.Object, bool, error) {
+	// no way to verify this, we rely on Deploy's cmd.Run() not failing
+	return nil, a.loaded, nil
+}
