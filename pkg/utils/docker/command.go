5de31c1b6cac78f317b3e16ec3e228b0bd9ee78e
Shane Utt <shaneutt@linux.com>
2021-12-01 16:45:47 -0500

    feat: add docker util to run priv cmds

---
+++ pkg/utils/docker/command.go
@@ -0,0 +1,31 @@
+package docker
+
+import (
+	"context"
+
+	"github.com/docker/docker/api/types"
+	dockerclient "github.com/docker/docker/client"
+)
+
+// RunPrivilegedCommand is a very basic and opinionated helper function which runs the
+// given command and arguments on the given container (by ID) privileged.
+func RunPrivilegedCommand(ctx context.Context, containerID, command string, args ...string) error {
+	// connect to the local docker env
+	dockerc, err := dockerclient.NewEnvClient() //nolint:staticcheck
+	if err != nil {
+		return err
+	}
+
+	// load the exec command for the container
+	execID, err := dockerc.ContainerExecCreate(ctx, containerID, types.ExecConfig{
+		User:       "0",
+		Privileged: true,
+		Cmd:        append([]string{command}, args...),
+	})
+	if err != nil {
+		return err
+	}
+
+	// run the command
+	return dockerc.ContainerExecStart(ctx, execID.ID, types.ExecStartCheck{})
+}
