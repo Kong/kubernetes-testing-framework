fe1f1f08759a8580473125cfe5a30ae1adcc884f
Yi Tao <tao.yi@konghq.com>
2022-09-23 11:56:08 +0800

    feat: load multiple images in loadimage addon

--- pkg/clusters/addons/loadimage/cluster_implementations.go
+++ pkg/clusters/addons/loadimage/cluster_implementations.go
@@ -10,11 +10,16 @@ import (
 )
 
 func (a *Addon) loadIntoKind(cluster clusters.Cluster) error {
+	if len(a.images) == 0 {
+		return fmt.Errorf("no images provided")
+	}
+
 	deployArgs := []string{
 		"load", "docker-image",
-		a.image,
 		"--name", cluster.Name(),
 	}
+
+	deployArgs = append(deployArgs, a.images...)
 
 	stderr := new(bytes.Buffer)
 	cmd := exec.Command("kind", deployArgs...)
