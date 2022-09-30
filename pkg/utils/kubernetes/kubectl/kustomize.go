b51d38366bf04d879078be57a76ca63e1ff80b4d
Travis Raines <571832+rainest@users.noreply.github.com>
2022-09-30 14:08:54 -0700

    chore: fix kustomization fields

--- pkg/utils/kubernetes/kubectl/kustomize.go
+++ pkg/utils/kubernetes/kubectl/kustomize.go
@@ -26,11 +26,11 @@ func GetKustomizedManifest(kustomization types.Kustomization, manifests ...io.Re
 		if err != nil {
 			return nil, err
 		}
-		err = os.WriteFile(filepath.Join(workDir, fmt.Sprintf("base_%d.yaml", i)), orig, 0o600) //nolint:gomnd
+		err = os.WriteFile(filepath.Join(workDir, fmt.Sprintf("resource_%d.yaml", i)), orig, 0o600) //nolint:gomnd
 		if err != nil {
 			return nil, err
 		}
-		kustomization.Resources = append(kustomization.Bases, fmt.Sprintf("base_%d.yaml", i))
+		kustomization.Resources = append(kustomization.Resources, fmt.Sprintf("resource_%d.yaml", i))
 	}
 	marshalled, err := yaml.Marshal(kustomization)
 	if err != nil {
