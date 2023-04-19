255b67ff20c7849cd1e549cb6a890641662cf73d
Yi Tao <tao.yi@konghq.com>
2023-04-19 15:59:38 +0800

    put back code for dumping kubectl get all and describe all

--- pkg/clusters/diagnostics.go
+++ pkg/clusters/diagnostics.go
@@ -1,10 +1,13 @@
 package clusters
 
 import (
+	"bytes"
 	"context"
 	"fmt"
 	"os"
+	"os/exec"
 	"path/filepath"
+	"strings"
 )
 
 // DumpDiagnostics gathers a wide range of generic, diagnostic information from the test cluster,
@@ -56,6 +59,21 @@ func DumpDiagnostics(ctx context.Context, c Cluster, meta string, outDir string)
 		}
 	}
 
+	err := DumpKubectlGetAllAndDescribeAll(ctx, c, meta, outDir)
+	// write errors if we failed to dump results of `kubectl get all` or `kubectl describe all`.
+	// in cases where kubernetes cluster may not be correctly created.
+	if err != nil {
+		kubectlErrorOut, openErr := os.OpenFile(filepath.Join(outDir, "kubectl_dump_error.txt"), os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0o644) //nolint:gomnd
+		if openErr != nil {
+			return outDir, openErr
+		}
+		defer kubectlErrorOut.Close()
+		_, writeErr := kubectlErrorOut.WriteString(err.Error())
+		if writeErr != nil {
+			return outDir, writeErr
+		}
+	}
+
 	// write the diagnostic metadata
 	metaOut, err := os.Create(filepath.Join(outDir, "meta.txt"))
 	if err != nil {
@@ -68,4 +86,68 @@ func DumpDiagnostics(ctx context.Context, c Cluster, meta string, outDir string)
 	}
 
 	return outDir, nil
+}
+
+// dump results of `kubectl get all` and `kubectl describe all`.
+// TODO: split results into one file per resource kind.
+func DumpKubectlGetAllAndDescribeAll(ctx context.Context, c Cluster, meta string, outDir string) error {
+	kubeconfig, err := TempKubeconfig(c)
+	if err != nil {
+		return err
+	}
+	defer os.Remove(kubeconfig.Name())
+
+	// kubectl api-resources --verbs=list --namespaced -o name  | xargs -n 1 kubectl get --show-kind --ignore-not-found -A -oyaml
+	// kubectl api-resources --verbs=list --namespaced -o name  | xargs -n 1 kubectl get --show-kind --ignore-not-found -A -oyaml
+	// aka "kubectl get all" and "kubectl describe all", but also gets CRs and cluster-scoped resouces
+
+	// create output file for get all and describe all.
+	getAllOut, err := os.OpenFile(filepath.Join(outDir, "kubectl_get_all.yaml"), os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0o644) //nolint:gomnd
+	if err != nil {
+		return err
+	}
+	defer getAllOut.Close()
+	describeAllOut, err := os.OpenFile(filepath.Join(outDir, "kubectl_describe_all.txt"), os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0o644) //nolint:gomnd
+	if err != nil {
+		return err
+	}
+	defer describeAllOut.Close()
+
+	var namespacedList bytes.Buffer
+	var clusterList bytes.Buffer
+	namespacedResources := exec.CommandContext(ctx, "kubectl", "--kubeconfig", kubeconfig.Name(), "api-resources", "--verbs=list", "--namespaced", "-o", "name") //nolint:gosec
+	namespacedResources.Stdout = &namespacedList
+	clusterResources := exec.CommandContext(ctx, "kubectl", "--kubeconfig", kubeconfig.Name(), "api-resources", "--verbs=list", "--namespaced=false", "-o", "name") //nolint:gosec
+	clusterResources.Stdout = &clusterList
+	if err := namespacedResources.Run(); err != nil {
+		return err
+	}
+	if err := clusterResources.Run(); err != nil {
+		return err
+	}
+	combinedList := strings.Split(namespacedList.String()+clusterList.String(), "\n")
+
+	// run kubectl get all and kubectl describe all for each resource.
+	for _, resource := range combinedList {
+		if resource == "" {
+			// unwanted artifact of the split
+			continue
+		}
+		var getErr bytes.Buffer
+		var descErr bytes.Buffer
+		resourceGet := exec.CommandContext(ctx, "kubectl", "--kubeconfig", kubeconfig.Name(), "get", "--show-kind", "--ignore-not-found", "-A", "-oyaml", resource) //nolint:gosec
+		resourceGet.Stdout = getAllOut
+		resourceGet.Stderr = &getErr
+		resourceDescribe := exec.CommandContext(ctx, "kubectl", "--kubeconfig", kubeconfig.Name(), "describe", "--all-namespaces", resource) //nolint:gosec
+		resourceDescribe.Stdout = describeAllOut
+		resourceDescribe.Stderr = &descErr
+		if err := resourceGet.Run(); err != nil {
+			return fmt.Errorf("could not get resources for cmd '%s': err %s, stderr: %s", resourceGet.String(), err, getErr.String())
+		}
+		if err := resourceDescribe.Run(); err != nil {
+			return fmt.Errorf("could not get resources for cmd '%s': err %s, stderr: %s", resourceDescribe.String(), err, descErr.String())
+		}
+	}
+
+	return nil
 }
