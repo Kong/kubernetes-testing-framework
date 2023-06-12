d5932069b2299927bf12e39f5830583fee519afc
Patryk Małek <patryk.malek@konghq.com>
2023-06-12 15:09:33 +0200

    tests(integration): run just 1 test for kind cluster WithClusterVersion() (#707)

--- test/integration/kind_version_test.go
+++ test/integration/kind_version_test.go
@@ -4,6 +4,7 @@
 package integration
 
 import (
+	"context"
 	"testing"
 	"time"
 
@@ -14,52 +15,27 @@ import (
 	"github.com/kong/kubernetes-testing-framework/pkg/clusters/types/kind"
 )
 
-func TestKindClusterOlderVersion(t *testing.T) {
+func TestKindWithClusterVersion(t *testing.T) {
 	t.Parallel()
 
-	clusterVersion := semver.MustParse("1.20.7")
+	clusterVersion, err := semver.Parse("1.26.4")
+	require.NoError(t, err)
 	t.Logf("deploying a kind cluster with kubernetes version %s", clusterVersion)
 	builder := kind.NewBuilder().WithClusterVersion(clusterVersion)
-	cluster, err := builder.Build(ctx)
+	cluster, err := builder.Build(context.Background())
 	require.NoError(t, err)
 
 	defer func() {
 		t.Logf("cleaning up kind cluster %s", cluster.Name())
-		assert.NoError(t, cluster.Cleanup(ctx))
+		// Don't use test context as it may be cancelled already
+		assert.NoError(t, cluster.Cleanup(context.Background()))
 	}()
 
 	t.Log("waiting for cluster api to become usable")
 	require.Eventually(t, func() bool {
 		_, err := cluster.Client().ServerVersion()
 		return err == nil
-	}, time.Minute*1, time.Second*1)
-
-	t.Logf("verifying that the created cluster is kubernetes version %s", clusterVersion)
-	serverVersion, err := cluster.Version()
-	require.NoError(t, err)
-	require.Equal(t, clusterVersion.String(), serverVersion.String())
-	require.True(t, clusterVersion.EQ(serverVersion))
-}
-
-func TestKindClusterNewerVersion(t *testing.T) {
-	t.Parallel()
-
-	clusterVersion := semver.MustParse("1.21.1")
-	t.Logf("deploying a kind cluster with kubernetes version %s", clusterVersion)
-	builder := kind.NewBuilder().WithClusterVersion(clusterVersion)
-	cluster, err := builder.Build(ctx)
-	require.NoError(t, err)
-
-	defer func() {
-		t.Logf("cleaning up kind cluster %s", cluster.Name())
-		assert.NoError(t, cluster.Cleanup(ctx))
-	}()
-
-	t.Log("waiting for cluster api to become usable")
-	require.Eventually(t, func() bool {
-		_, err := cluster.Client().ServerVersion()
-		return err == nil
-	}, time.Minute*1, time.Second*1)
+	}, time.Minute, time.Second)
 
 	t.Logf("verifying that the created cluster is kubernetes version %s", clusterVersion)
 	serverVersion, err := cluster.Version()
